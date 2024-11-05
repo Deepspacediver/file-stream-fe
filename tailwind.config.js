a841437ef0dc4acbf64cadc9eb1f3c63f404858e
lukasz-mac <luka.koscik@gmail.com>
2024-11-05 09:05:02 +0100

    extend tailwind config styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "col-white": "#FFFFFF",
+        "col-purple": "#53118F",
+        "col-black": "000000",
+      },
+      backgroundImage: {
+        gradient: `radial-gradient(circle farthest-corner at 10% 20%,  rgba(100,43,115,1) 0%, rgba(4,0,4,1) 90% )`,
+      },
+    },
   },
   plugins: [],
 };
