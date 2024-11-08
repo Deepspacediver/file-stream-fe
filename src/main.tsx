40fefa8355ece9978bd7f4c02d346795bd056487
lukasz-mac <luka.koscik@gmail.com>
2024-11-08 18:31:33 +0100

    add tenstack query with queryclient config

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import "@/assets/styles/reset.css";
 import "@/assets/styles/index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { routes } from "./router/routes";
+import { routes } from "@/router/routes";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "@/lib/query-client";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
