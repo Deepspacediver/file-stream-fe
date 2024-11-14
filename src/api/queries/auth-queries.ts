bf6ac583dad510b35829642b9bdce94ae846c6e2
lukasz-mac <luka.koscik@gmail.com>
2024-11-14 15:59:21 +0100

    update use login to update user & logged state, add config options to useGetLoggedUserData

--- src/api/queries/auth-queries.ts
+++ src/api/queries/auth-queries.ts
@@ -1,13 +1,19 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { getLoggedUserData, login } from "../services/auth-services";
+import { setIsLogged } from "@/helpers/local-storage-helpers";
+import { useContext } from "react";
+import { UserContext } from "@/contexts/user-context";
 
 const BASE_KEY = "auth";
 
 export const useLogin = () => {
+  const { setUser } = useContext(UserContext);
   const queryClient = useQueryClient();
   const { mutate: loginUser } = useMutation({
     mutationFn: login,
-    onSuccess: () => {
+    onSuccess: (user) => {
+      setIsLogged();
+      setUser(user);
       queryClient.invalidateQueries({ queryKey: [BASE_KEY] });
     },
   });
@@ -15,10 +21,12 @@ export const useLogin = () => {
   return { loginUser };
 };
 
-export const useGetLoggedUserData = () => {
+export const useGetLoggedUserData = (isDisabled?: boolean) => {
   const { data: user, isLoading } = useQuery({
     queryFn: getLoggedUserData,
     queryKey: [BASE_KEY],
+    enabled: !isDisabled,
+    retry: 0,
   });
 
   return {
