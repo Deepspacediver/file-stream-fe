2b0bb634e7a53fa55b037319f7ec2b9a846b91bb
lukasz-mac <luka.koscik@gmail.com>
2024-11-19 16:32:51 +0100

    redirect on registration/login

--- src/api/queries/users-queries.ts
+++ src/api/queries/users-queries.ts
@@ -1,14 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { registerUser } from "../services/users-services";
+import { useNavigate } from "react-router-dom";
 
 const BASE_KEY = "users";
 
 export const useRegisterUser = () => {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
   const { mutate: createUser, isPending } = useMutation({
     mutationFn: registerUser,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [BASE_KEY] });
+      navigate("/");
     },
   });
 
