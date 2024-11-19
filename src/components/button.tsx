5f18451f37ce02b1db96e5c690145a301e901367
lukasz-mac <luka.koscik@gmail.com>
2024-11-19 16:33:09 +0100

    add twMerge to button and change styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,14 +1,21 @@
+import clsx from "clsx";
 import { ButtonHTMLAttributes, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 type ButtonProps = {
   children: ReactNode;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function Button({ children, ...rest }: ButtonProps) {
+export default function Button({ children, className, ...rest }: ButtonProps) {
   return (
     <button
       {...rest}
-      className="mx-auto magnify p-2 bg-col-white text-col-black rounded-md w-fit min-w-32"
+      className={twMerge(
+        clsx(
+          "magnify p-2 bg-col-white text-col-black rounded-md w-fit min-w-32",
+          className
+        )
+      )}
     >
       {children}
     </button>
