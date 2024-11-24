cbce718d123be60744176cc8daf56655cf3eb9ff
lukasz-mac <luka.koscik@gmail.com>
2024-11-24 14:03:21 +0100

    add node types

---
+++ src/types/node-types.ts
@@ -0,0 +1,16 @@
+export enum NodeTypes {
+  FOLDER = "FOLDER",
+  FILE = "FILE",
+}
+
+export type CreateFolder = {
+  name: string;
+  parentNodeId: number;
+  type: NodeTypes.FOLDER;
+  userId?: number;
+};
+
+export type UserFolderResponse = {
+  node_id: number;
+  name: string;
+};
