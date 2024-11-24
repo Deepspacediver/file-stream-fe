ad90f64323c99251c17f73bff7fa0c2af5ccea09
lukasz-mac <luka.koscik@gmail.com>
2024-11-24 12:22:05 +0100

    login user on successful registration query

--- src/api/queries/users-queries.ts
+++ src/api/queries/users-queries.ts
@@ -1,15 +1,21 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { registerUser } from "../services/users-services";
 import { useNavigate } from "react-router-dom";
+import { setIsLogged } from "@/helpers/local-storage-helpers";
+import { useContext } from "react";
+import { UserContext } from "@/contexts/user-context";
 
 const BASE_KEY = "users";
 
 export const useRegisterUser = () => {
+  const { setUser } = useContext(UserContext);
   const navigate = useNavigate();
   const queryClient = useQueryClient();
   const { mutate: createUser, isPending } = useMutation({
     mutationFn: registerUser,
-    onSuccess: () => {
+    onSuccess: (user) => {
+      setUser(user);
+      setIsLogged();
       queryClient.invalidateQueries({ queryKey: [BASE_KEY] });
       navigate("/");
     },
