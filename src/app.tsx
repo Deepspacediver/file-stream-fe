1b805e0b312abee871ff889aa02512433d2a3665
lukasz-mac <luka.koscik@gmail.com>
2024-11-27 16:26:05 +0100

    add loader to app

--- src/app.tsx
+++ src/app.tsx
@@ -8,12 +8,15 @@ import { UserContext } from "@/contexts/user-context";
 import AxiosWrapper from "./components/axios-wrapper";
 import "@/assets/styles/reset.css";
 import "@/assets/styles/index.css";
+import Loader from "./components/loader";
 
 const router = createBrowserRouter(routes);
 
 export default function App() {
   const [user, setUser] = useState<UserWithoutPassword | null>(null);
-  const { user: userResponse } = useGetLoggedUserData(!!user || !getIsLogged());
+  const { user: userResponse, isLoading } = useGetLoggedUserData(
+    !!user || !getIsLogged()
+  );
 
   useEffect(() => {
     if (userResponse) {
@@ -21,6 +24,10 @@ export default function App() {
       setUser(userResponse);
     }
   }, [userResponse]);
+
+  if (isLoading) {
+    return <Loader isFullScreen />;
+  }
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
