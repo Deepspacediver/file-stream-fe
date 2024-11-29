bff9d20ef8ae39b07a45c9a8ea4a4df9a03b9db8
Lukasz <luka.koscik@gmail.com>
2024-11-29 19:06:16 +0100

    [FEATURE-9] Protected routes (#11)
    * update user type with undefined as initial state & null as state for user fater logout
    
    * add protected route component

--- src/contexts/user-context.ts
+++ src/contexts/user-context.ts
@@ -2,8 +2,8 @@ import { createContext, Dispatch, SetStateAction } from "react";
 import { UserWithoutPassword } from "@/types/user-types";
 
 export type UserContext = {
-  user: UserWithoutPassword | null;
-  setUser: Dispatch<SetStateAction<UserWithoutPassword | null>>;
+  user: UserWithoutPassword | null | undefined;
+  setUser: Dispatch<SetStateAction<UserWithoutPassword | null | undefined>>;
 };
 
 export const UserContext = createContext({} as UserContext);
