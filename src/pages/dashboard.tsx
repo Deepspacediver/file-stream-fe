0a8e686d2e9a41e517bf23f7d05bdecc0bead3cb
lukasz-mac <luka.koscik@gmail.com>
2024-12-01 13:56:38 +0100

    fill dashboard with folder previews

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,31 @@
+import { useGetUserFolders } from "@/api/queries/users-queries";
+import FolderPreviewItem from "@/components/folder-preview-item";
+import Loader from "@/components/loader";
+import { UserContext } from "@/contexts/user-context";
+import { useContext } from "react";
+
 export default function Dasboard() {
-  return <div></div>;
+  const { user } = useContext(UserContext);
+  const { folders, isLoading } = useGetUserFolders(user?.userId);
+
+  if (isLoading) {
+    return <Loader isCentered />;
+  }
+
+  return (
+    <div className="flex flex-col pt-4 gap-4 max-w-[1200px] mx-auto w-full">
+      <h2 className="font-medium text-2xl self-center">
+        Welcome to your drive
+      </h2>
+      <div className="grid auto-fill-cols justify-center items-center gap-4 lg:gap-6 2xl:gap-8">
+        {!folders || !folders?.length ? (
+          <p>No folders found </p>
+        ) : (
+          folders.map((folder) => (
+            <FolderPreviewItem key={folder.nodeId} folder={folder} />
+          ))
+        )}
+      </div>
+    </div>
+  );
 }
