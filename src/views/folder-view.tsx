c10092cc13f16373340af46447fcf323e1a39e4f
lukasz-mac <luka.koscik@gmail.com>
2024-12-02 16:04:59 +0100

    add fetching folder content to folder view

--- src/views/folder-view.tsx
+++ src/views/folder-view.tsx
@@ -1,3 +1,22 @@
+import { useGetFolderContent } from "@/api/queries/users-queries";
+import Loader from "@/components/loader";
+import { UserContext } from "@/contexts/user-context";
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+
 export default function FolderView() {
-  return <div></div>;
+  const params = useParams();
+  const { folderId } = params;
+  const { user } = useContext(UserContext);
+  const userId = user ? +user.userId : null;
+  const nodeId = folderId ? +folderId : null;
+
+  const { folderContent, isLoading } = useGetFolderContent({ userId, nodeId });
+
+  if (isLoading) {
+    return <Loader isCentered />;
+  }
+  console.log({ userId, nodeId, folderContent });
+
+  return <div>{folderId}</div>;
 }
