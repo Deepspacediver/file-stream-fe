09e7e0b5744db310dc5fd2454a827fec8fb30d09
lukasz-mac <luka.koscik@gmail.com>
2024-12-03 18:26:42 +0100

    update capitalize helper

--- src/helpers/string-helpers.ts
+++ src/helpers/string-helpers.ts
@@ -5,5 +5,5 @@ export const capitalizeString = (data: string) => {
   if (data.length === 1) {
     return data.toUpperCase();
   }
-  return data[0].toUpperCase().concat(data.slice(1));
+  return data[0].toUpperCase().concat(data.slice(1).toLowerCase());
 };
