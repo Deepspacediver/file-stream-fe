36e5dd5b87badc7326d29d7e73310b53079d3b29
lukasz-mac <luka.koscik@gmail.com>
2024-12-04 14:32:53 +0100

    momoize columns, add CopyText to copy file/folder urls, add sorting state to table, update heading/row styles, redirect to file/folder link on row click, add arrow state for sorting

--- src/components/folder-table.tsx
+++ src/components/folder-table.tsx
@@ -5,61 +5,71 @@ import {
   flexRender,
   getCoreRowModel,
   useReactTable,
+  SortingState,
+  getSortedRowModel,
 } from "@tanstack/react-table";
-import CustomNavlink from "./custom-navlink";
 import FolderIcon from "@/assets/icons/folder-white-outline.svg?react";
 import FileIcon from "@/assets/icons/file-icon.svg?react";
+import { useMemo, useState } from "react";
+import clsx from "clsx";
+import { useNavigate } from "react-router-dom";
+import CopyText from "./copy-text";
 
 type FolderTableProps = {
   folderContent: FolderContent[];
 };
 
 export default function FolderTable({ folderContent }: FolderTableProps) {
+  const navigate = useNavigate();
+  const [sorting, setSorting] = useState<SortingState>([]);
   const columnHelper = createColumnHelper<FolderContent>();
-  const columns = [
-    columnHelper.accessor("name", {
-      header: "Name",
-      cell: ({ row, getValue }) => {
-        const cellData = getValue();
-        const isFolder = row.original.type === NodeTypes.FOLDER;
-        const iconDimensionStyle = "w-6 h-6 lg:w-7 lg:h-7";
-        const icon = isFolder ? (
-          <FolderIcon className={`text-col-black ${iconDimensionStyle}`} />
-        ) : (
-          <FileIcon className={`${iconDimensionStyle}`} />
-        );
-        return (
-          <span className="flex items-center gap-2">
-            {icon}
-            {cellData}
-          </span>
-        );
-      },
-    }),
-    columnHelper.accessor("type", {
-      header: "Type",
-      cell: (info) => capitalizeString(info.getValue()),
-    }),
-    columnHelper.accessor("fileLink", {
-      header: "Link",
-      cell: ({ row, getValue }) => {
-        const cellData = getValue();
-        const folderLink = `/folders/${row.original.nodeId}`;
-        return cellData ? (
-          <a className="underline" href={cellData}>
-            Link
-          </a>
-        ) : (
-          <CustomNavlink to={folderLink}>Link</CustomNavlink>
-        );
-      },
-    }),
-  ];
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor("name", {
+        header: "Name",
+        cell: ({ row, getValue }) => {
+          const cellData = getValue();
+          const isFolder = row.original.type === NodeTypes.FOLDER;
+          const iconDimensionStyle = "w-6 h-6 lg:w-7 lg:h-7";
+          const icon = isFolder ? (
+            <FolderIcon className={`text-col-black ${iconDimensionStyle}`} />
+          ) : (
+            <FileIcon className={`${iconDimensionStyle}`} />
+          );
+          return (
+            <span className="flex items-center gap-2">
+              {icon}
+              {cellData}
+            </span>
+          );
+        },
+      }),
+      columnHelper.accessor("type", {
+        header: "Type",
+        cell: (info) => capitalizeString(info.getValue()),
+      }),
+      columnHelper.accessor("fileLink", {
+        header: "Link",
+        enableSorting: false,
+        cell: ({ row, getValue }) => {
+          const cellData = getValue();
+          const folderLink = `${location.host}/folders/${row.original.nodeId}`;
+          return <CopyText text={cellData ?? folderLink} />;
+        },
+      }),
+    ],
+    [columnHelper]
+  );
 
   const table = useReactTable({
     data: folderContent,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onSortingChange: setSorting,
+    state: {
+      sorting,
+    },
   });
 
   return (
@@ -70,13 +80,25 @@ export default function FolderTable({ folderContent }: FolderTableProps) {
             <tr key={headerGroup.id} className=" border-b-2 border-gray-300">
               {headerGroup.headers.map((header) => {
                 return (
-                  <th className="p-2" key={header.id}>
+                  <th
+                    onClick={header.column.getToggleSortingHandler()}
+                    className={`p-2 relative ${
+                      header.column.getCanSort() && "hover:cursor-pointer"
+                    }`}
+                    key={header.id}
+                  >
                     {header.isPlaceholder
                       ? null
                       : flexRender(
                           header.column.columnDef.header,
                           header.getContext()
                         )}
+                    <span className="absolute ml-2 bottom-1 text-lg">
+                      {{
+                        asc: "  \u2191",
+                        desc: "  \u2193",
+                      }[header.column.getIsSorted() as string] ?? null}
+                    </span>
                   </th>
                 );
               })}
@@ -87,7 +109,23 @@ export default function FolderTable({ folderContent }: FolderTableProps) {
       <tbody>
         {table.getRowModel().rows.map((row) => {
           return (
-            <tr key={row.id} className="border-b-2 border-gray-300">
+            <tr
+              key={row.id}
+              className={clsx(
+                "border-b-2 border-gray-300 focus:bg-col-black ",
+                "cursor-pointer hover:bg-gray-200"
+              )}
+              onClick={() => {
+                if (row.original.type === NodeTypes.FOLDER) {
+                  navigate(`/folders/${row.original.nodeId}`);
+                  return;
+                }
+                const fileLink = row.original.fileLink;
+                if (fileLink) {
+                  window.open(fileLink, "_blank");
+                }
+              }}
+            >
               {row.getVisibleCells().map((cell) => {
                 return (
                   <td className="text-center p-2" key={cell.id}>
