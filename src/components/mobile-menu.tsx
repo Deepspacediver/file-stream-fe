7f3209c7095a2deef2a863b50ef28ffdbe038ae5
lukasz-mac <luka.koscik@gmail.com>
2024-12-16 17:03:34 +0100

    add FolderTreeWrapper to menus

--- src/components/mobile-menu.tsx
+++ src/components/mobile-menu.tsx
@@ -2,13 +2,14 @@ import clsx from "clsx";
 import CustomNavlink from "./custom-navlink";
 import CloseIcon from "@/assets/icons/close-icon.svg?react";
 import { FolderTree } from "@/types/node-types";
-import FolderMenuTree from "./folder-menu-tree";
+import FolderTreeWrapper from "./folder-tree-wrapper";
 
 type MobileMenuProps = {
   className?: string;
   handleIsMenuVisibility: () => void;
   isOpen: boolean;
   folderTree?: FolderTree;
+  sharedFolderTree?: FolderTree;
 };
 
 export default function MobileMenu({
@@ -16,27 +17,31 @@ export default function MobileMenu({
   handleIsMenuVisibility,
   className,
   folderTree,
+  sharedFolderTree,
 }: MobileMenuProps) {
   return (
     <aside
       className={clsx(
-        "bg-gradient-vertical lg:hidden w-full max-w-80 absolute top-0 h-full z-10 transition-all transform -translate-x-full",
+        "text-col-white bg-gradient-vertical lg:hidden w-full max-w-80 absolute top-0 h-full z-10 transition-all transform -translate-x-full",
         isOpen && "translate-x-0",
         className
       )}
     >
       <div className="flex mx-auto items-center p-5 gap-2">
         <CustomNavlink to="/" className="no-underline">
-          <h1 className="text-4xl font-medium text-col-white">FileStream</h1>
+          <h1 className="text-4xl font-medium ">FileStream</h1>
         </CustomNavlink>
         <CloseIcon
-          className="w-10 h-10 text-col-white ml-auto hover:cursor-pointer"
+          className="w-10 h-10  ml-auto hover:cursor-pointer"
           onClick={() => {
             handleIsMenuVisibility();
           }}
         />
       </div>
-      {folderTree && <FolderMenuTree folder={folderTree} />}
+      {folderTree && <FolderTreeWrapper folderTree={folderTree} />}
+      {sharedFolderTree && (
+        <FolderTreeWrapper folderTree={sharedFolderTree} isShared />
+      )}
     </aside>
   );
 }
