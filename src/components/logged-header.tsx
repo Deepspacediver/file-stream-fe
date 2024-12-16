20ba4aeb3667fe90b91bae03ad87226f34497daf
lukasz-mac <luka.koscik@gmail.com>
2024-12-16 09:36:48 +0100

    hide NodeButton & UserMenu from header when user is unlogged (case for shared folder view)

--- src/components/logged-header.tsx
+++ src/components/logged-header.tsx
@@ -1,6 +1,8 @@
 import HamburgerIcon from "@/assets/icons/hamburger-menu-icon.svg?react";
 import UserMenu from "./user-menu";
 import NodeButton from "./node-button";
+import { useContext } from "react";
+import { UserContext } from "@/contexts/user-context";
 
 type LoggedHeaderProps = {
   handleMenuVisibility: () => void;
@@ -9,16 +11,22 @@ type LoggedHeaderProps = {
 export default function LoggedHeader({
   handleMenuVisibility,
 }: LoggedHeaderProps) {
+  const { user } = useContext(UserContext);
+  const isLogged = !!user;
   return (
-    <header className="flex flex-row w-full h-18 items-center gap-1 border-b-2 border-col-gray p-2">
+    <header className="flex flex-row w-full min-h-16 h-18 items-center gap-1 border-b-2 border-col-gray p-2">
       <HamburgerIcon
         className="w-5 h-5 mx-0 mr-auto lg:hidden min-w-fit hover:cursor-pointer"
         onClick={() => {
           handleMenuVisibility();
         }}
       />
-      <NodeButton />
-      <UserMenu />
+      {isLogged && (
+        <>
+          <NodeButton />
+          <UserMenu />
+        </>
+      )}
     </header>
   );
 }
