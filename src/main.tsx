fb73c554db6913bc947f531b5478855551f6bc1a
lukasz-mac <luka.koscik@gmail.com>
2024-12-17 19:54:56 +0100

    add toast container to main

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import App from "./app";
 import { StrictMode } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./lib/query-client";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <ToastContainer />
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
